06fc3df97de0bd8a3f0578d1529d1f85688cdc78
Oladapo Omonayajo <oladapo.omonayajo@lazada.com.ph>
2018-07-31 15:37:53 +0800

    Add a bit of background color to offset card design

--- lib/home.dart
+++ lib/home.dart
@@ -36,35 +36,40 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        children: <Widget>[
-          Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Card Title'),
-                      SizedBox(height: 8.0,),
-                      Text('Secondary Text')
-                    ],
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[300]
+        ),
+        child: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: <Widget>[
+            Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Image.asset('assets/diamond.png'),
                   ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Card Title'),
+                        SizedBox(height: 8.0,),
+                        Text('Secondary Text')
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
