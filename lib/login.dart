d578ca23b5e5768f8c562b67b29d97cd3dc6441c
Oladapo Omonayajo <oladapo.omonayajo@lazada.com.ph>
2018-07-31 14:58:54 +0800

    Added first page

---
+++ lib/login.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginWidget extends StatefulWidget {
+  @override
+  _LoginWidgetState createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/diamond.png'),
+                SizedBox(height: 16.0),
+                Text('SHRINE')
+              ],
+            ),
+            SizedBox(height: 120.0)
+          ],
+        ),
+      ),
+    );
+  }
+}
