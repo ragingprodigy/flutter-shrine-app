dbcce3d8aeb594ca8ce31742b05ded72d39f3461
Oladapo Omonayajo <oladapo.omonayajo@lazada.com.ph>
2018-07-31 16:04:19 +0800

    Show a dynamic list of products

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/data.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -44,32 +48,62 @@ class HomePage extends StatelessWidget {
           crossAxisCount: 2,
           padding: EdgeInsets.all(16.0),
           childAspectRatio: 8.0 / 9.0,
-          children: <Widget>[
-            Card(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  AspectRatio(
-                    aspectRatio: 18.0 / 11.0,
-                    child: Image.asset('assets/diamond.png'),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Card Title'),
-                        SizedBox(height: 8.0,),
-                        Text('Secondary Text')
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+          children: _buildGridCards(context),
         ),
       )
     );
+  }
+
+  List<Card> _buildGridCards(context) {
+    List<Product> products = getProducts(Category.all);
+
+    if (products == null || products.isEmpty) {
+      return const <Card>[];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((Product product) => Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 18.0 / 11.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.green
+              ),
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    product.name,
+                    style: theme.textTheme.title,
+                    maxLines: 1,
+                  ),
+                  SizedBox(height: 8.0,),
+                  Text(
+                    formatter.format(product.price),
+                    style: theme.textTheme.body2,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    )).toList();
   }
 }
